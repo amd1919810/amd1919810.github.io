b22daa8d0ca9677e64740ce3c97ee4179b58f95e
auz <122498815+amd1919810@users.noreply.github.com>
2023-02-17 21:48:43 +0800

    Update time.js

--- saiban/time.js
+++ saiban/time.js
@@ -1,1 +1,22 @@
-
+var d = new Date();
+var time = d.getHours();
+if (time<=6 && time>=0)
+{
+	document.write("<b>凌晨好</b>");
+}
+else if (time<=10 && time>=6)
+{
+	document.write("<b>早上好</b>");
+}
+else if (time==12)
+{
+	document.write("<b>中午好</b>");
+}
+else if (time>=12 && time<=18)
+{
+	document.write("<b>下午好</b>");
+}
+else
+{
+	document.write("<b>晚上好</b>");
+}
