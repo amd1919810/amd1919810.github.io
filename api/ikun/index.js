8ac04d396a949d5785a0f385cbeb6d60b0699016
auz <122498815+amd1919810@users.noreply.github.com>
2023-03-08 19:09:01 +0800

    Delete api directory

--- api/ikun/index.js
+++
@@ -1,9 +0,0 @@
-var audios = ['1.mp3', '2.mp3', '3.mp3'];
-
-var generateRandomAudio = function(audios) {
-
-  var audio = new Audio(audios[Math.floor(Math.random() * audios.length)]);
-
-  return audio;
-
-}
